cb988c1a441c80565552cb9892607cdc8e29a7b3
AKotov-dev <alex_q_2000@mail.ru>
2021-08-08 20:28:24 +0300

    droidcam-resolution.sh for v1.8.0-3

--- scripts/3/droidcam-resolution.sh
+++ scripts/3/droidcam-resolution.sh
@@ -2,10 +2,8 @@
 
 # DroidCam Resolution
 # Requires pkexec and zenity
-# Author: Alex Kotov aka alex_q_2000 (C) 2021
-# License: GPLv3
 
-if [ "$EUID" != "0" ] ; then pkexec "$0"; exit; fi;
+if [ "$EUID" != "0" ]; then pkexec "$0"; exit; fi;
 
 w=$(zenity --list --radiolist --title="DroidCam Resolution v0.1" --width=400 --height=270 \
 --column="#" --column="Width" --column="Height" 640 640 "480" 960 960 "720" 1280 1280 "720" 1920 1920 "1080")
@@ -31,18 +29,18 @@ esac
 sed -i "s/width=*[0-9]*/width=$w/g" /etc/modprobe.d/droidcam.conf
 sed -i "s/height=*[0-9]*/height=$h/g" /etc/modprobe.d/droidcam.conf
 
-# Kill droidcam if running & v4l2loopback_dc/videodev is busy
+# Kill droidcam if running & v4l2loopback_dc is busy
 for ((i=1; i < 10; i++)); do
    if [[ $(pidof droidcam) ]]; then
      killall droidcam; sleep 1
    else
      break
    fi
-done | zenity --progress --pulsate --auto-close
+done
 
-rmmod -f v4l2loopback_dc && rmmod -f videodev && modprobe videodev && modprobe v4l2loopback_dc
+rmmod -f v4l2loopback_dc; modprobe v4l2loopback_dc
 
 zenity --info --no-wrap --text="Resolution changed: ${w}x${h}\n\n\
-v4l2loopback_dc\/videodev modules has been reloaded"
+v4l2loopback_dc module has been reloaded"
 
 exit 0;
