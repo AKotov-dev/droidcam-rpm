d245561f5a045347ceeb428a99ffabb866983350
AKotov-dev <alex_q_2000@mail.ru>
2021-08-11 13:06:55 +0300

    DroidCam Resolution-v0.5 (applying parameters after a reboot)

--- scripts/droidcam-resolution.sh
+++ scripts/droidcam-resolution.sh
@@ -5,7 +5,7 @@
 
 if [ "$EUID" != "0" ]; then pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY "$0"; exit; fi;
 
-w=$(zenity --list --radiolist --title="DroidCam Resolution v0.4" --width=400 --height=270 \
+w=$(zenity --list --radiolist --title="DroidCam Resolution v0.5" --width=400 --height=270 \
 --column="#" --column="Width" --column="Height" \
 $([[ $(grep 640 /etc/modprobe.d/droidcam.conf) ]] && echo 'TRUE' || echo 'FALSE')  640 480 \
 $([[ $(grep 960 /etc/modprobe.d/droidcam.conf) ]] && echo 'TRUE' || echo 'FALSE')  960 720 \
@@ -34,20 +34,18 @@ sed -i "s/width=*[0-9]*/width=$w/g" /etc/modprobe.d/droidcam.conf
 sed -i "s/height=*[0-9]*/height=$h/g" /etc/modprobe.d/droidcam.conf
 
 # Kill droidcam if running & v4l2loopback_dc is busy
-for ((i=1; i < 10; i++)); do
-   if [[ $(pidof droidcam) ]]; then
-     killall droidcam; killall droidcam-cli; sleep 1
-   else
-     break
-   fi
-done | zenity --title="droidcam termination..." --progress --pulsate --auto-close
+#for ((i=1; i < 10; i++)); do
+#   if [[ $(pidof droidcam) ]]; then
+#     killall droidcam; killall droidcam-cli; sleep 1
+#   else
+#     break
+#   fi
+#done | zenity --title="droidcam termination..." --progress --pulsate --auto-close
 
 # Reload v4l2loopback_dc
-adb kill-server && adb start-server; rmmod -f v4l2loopback_dc; modprobe v4l2loopback_dc
+#adb kill-server && adb start-server; rmmod -f v4l2loopback_dc; modprobe v4l2loopback_dc
 
 zenity --info --no-wrap --text="Resolution changed: ${w}x${h}\n\n\
-Module v4l2loopback_dc has been reloaded\n\n\
-Don't forget to change the camera resolution\n\
-in your smartphone..."
+The new parameters will work after the reboot..."
 
 exit 0;
